ded2103444eedb707976e0ad8bea18d3553cb897
princeraj2003 <126746978+princeraj2003@users.noreply.github.com>
2023-09-15 19:36:42 +0530

    Update apiSlice.js

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,7 +1,7 @@
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseURI = 'http://localhost:8080';
+const baseURI = 'https://expensetrackerbackened.onrender.com';
 
 export const apiSlice = createApi({
     baseQuery : fetchBaseQuery({ baseUrl : baseURI}),
